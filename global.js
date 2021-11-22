38359eb95d304625506cc62af5e8773c7ff7c7ef
Mongus <mongus.c@gmail.com>
2021-11-22 12:24:45 +0900

    db connection string edit

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 const config = {
     MONGO_CONNECTION_STRING: 'mongodb+srv://mongus:1394218v@cluster0.scinh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    MONGO_DB_NAME: 'cloudTest',
+    MONGO_DB_NAME: 'hz-test-app',
     SECRET: 'SeCrEtKeYfOrHaShInG',
 }
 
